a49159080d825dc653c75961726fcf1203728a9e
Kamanpreet Singh <kskaman@gmail.com>
2024-07-29 21:31:05 -0230

    first commit

---
+++ 03 - Programming a server with Node JS and Express/part3/backend/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+let notes = [
+  { id: 1, content: "HTML is easy", important: true },
+  { id: 2, content: "Browser can execute only JavaScript", important: false },
+  { id: 3, content: "GET and POST are the most important methods of HTTP protocol", important: true }
+];
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // Specify your frontend's URL
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+}
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+  return maxId + 1;
+}
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
